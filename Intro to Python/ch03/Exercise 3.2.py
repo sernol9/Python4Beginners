78e2ff72389877f52b44b53847c092c47e711642
sernol9 <61778699+sernol9@users.noreply.github.com>
2020-03-24 10:41:51 +0800

    Create GA

---
+++ Intro to Python/ch03/Exercise 3.2.py
@@ -0,0 +1,9 @@
+'''
+ # @ Author: Olivia Serna
+ # @ Create Time: 2020-03-23 17:42:35
+ # @ Modified by: Olivia Serna
+ # @ Modified time: 2020-03-23 17:45:06
+ # @ Description: What is wrong with the following code?
+ '''
+a = b = 7
+print('a =', a, '\nb =', b)
---
+++ Intro to Python/ch03/Exercise 3.4.py
@@ -0,0 +1,11 @@
+'''
+ # @ Author: Olivia Serna
+ # @ Create Time: 2020-03-23 18:18:19
+ # @ Modified by: Olivia Serna
+ # @ Modified time: 2020-03-23 18:18:24
+ # @ Description: Fill in the missing code
+ '''
+for row in range(2):
+    for column in range (7):
+        print ('@',end='')
+    print()
